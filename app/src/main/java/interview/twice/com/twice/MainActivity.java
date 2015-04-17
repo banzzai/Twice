682e4f3b823d9bcf0f284cb5baafc728dc6bbe15
banzzai <gregory.jourdan@gmail.com>
2015-04-16 22:38:12 -0700

    Added the testing for words.

--- app/src/main/java/interview/twice/com/twice/MainActivity.java
+++ app/src/main/java/interview/twice/com/twice/MainActivity.java
@@ -1,7 +1,6 @@
 package interview.twice.com.twice;
 
 import android.app.Activity;
-import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
@@ -13,17 +12,39 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.HashSet;
 
 public class MainActivity extends Activity {
     private EditText mUserInput;
     private TextView mDisplay;
+    private TextView mResults;
 
-    private Map<String, Boolean> mDictionary = new HashMap<String, Boolean>();
+    private static HashSet<String> mDictionary = new HashSet<String>();
     private boolean mDictionaryReady = false;
+
+    // As explained below, taking small shortcuts with how I'm treating the dictionary as I want to
+    // focus on the solving part of the exercise. Loading the dictionary could use some more work.
     private static final int WORDS_IN_LANGUAGE = 172820;
+
+    // Set of the solutions found
+    private static HashSet<String> mSolutions = new HashSet<String>();
+
+    static void permute(int level, String permuted,
+                        boolean used[], String original) {
+        int length = original.length();
+        if (level == length) {
+            System.out.println(permuted);
+        } else {
+            for (int i = 0; i < length; i++) {
+                if (!used[i]) {
+                    used[i] = true;
+                    permute(level + 1, permuted + original.charAt(i),
+                            used, original);
+                    used[i] = false;
+                }
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,11 +53,26 @@ public class MainActivity extends Activity {
 
         mUserInput = (EditText) findViewById(R.id.textInput);
         mDisplay = (TextView) findViewById(R.id.display);
+        mResults = (TextView) findViewById(R.id.results);
 
         initDictionary();
         display(getString(R.string.loading));
     }
 
+    // Show a message on screen
+    private void display(final String string)
+    {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                mDisplay.setText(string);
+            }
+        });
+    }
+
+    // My understanding is that loading the dictionary wasn't quite the point of the exercise.
+    // With more time I would more likely look for a more efficient method, perhaps look for a file
+    // format that would be easier to be deserialized into a tree map.
     private void initDictionary()
     {
         new AsyncTask<Void, Integer, Boolean>()
@@ -54,7 +90,7 @@ public class MainActivity extends Activity {
                 {
                     while (( line = bufferReader.readLine()) != null)
                     {
-                        mDictionary.put(line, true);
+                        mDictionary.add(line);
                         progressInLines++;
 
                         if (Math.floor(100*progressInLines/WORDS_IN_LANGUAGE) > progressInPercents)
@@ -102,21 +138,18 @@ public class MainActivity extends Activity {
      */
     public void findWords(final View view)
     {
+        final long startTime = System.currentTimeMillis();
         if (mDictionaryReady)
         {
             final String inputString = mUserInput.getText().toString();
 
-            // Adding all letters to an array
-            if (!inputString.isEmpty()) {
-                ArrayList<Character> letters = new ArrayList<Character>();
-                // We are going to ignore the non letter characters
-                for (int i = 0; i < inputString.length(); i++) {
-                    final char character = inputString.charAt(i);
-                    if (Character.isLetter(character)) {
-                        letters.add(character);
-                    }
-                }
-            }
+            mSolutions.clear();
+
+            computePermutations(inputString);
+            displayWords();
+
+            final long totalTime = (System.currentTimeMillis() - startTime) / 1000;
+            mResults.setText(getString(R.string.result_count, mSolutions.size(), totalTime));
         }
         else
         {
@@ -124,13 +157,65 @@ public class MainActivity extends Activity {
         }
     }
 
-    private void display(final String string)
+    // Finds all permutations of letters, which are potential words
+    public static void computePermutations(String str)
     {
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                mDisplay.setText(string);
+        testPermutation("", str);
+    }
+
+    /**
+     * Pretty good snipet from
+     * http://stackoverflow.com/questions/4240080/generating-all-permutations-of-a-given-string
+     * My own solution would probably have been slightly inferior.
+     *
+     * @param prefix Part of the word that won't change in this tree of the recursion
+     * @param str End of the string that still has to be "permuted"
+     **/
+    private static void testPermutation(String prefix, String str)
+    {
+        int n = str.length();
+        if (n == 0)
+        {
+            // This is a permutation, see if it's a word
+            checkIfWord(prefix);
+        }
+        else
+        {
+            for (int i = 0; i < n; i++)
+            {
+                // While going through the characters in str, take one at random and place it first
+                // right behind the prefix, then iterate on that new string, i.e "permute" it.
+                testPermutation(prefix + str.charAt(i), str.substring(0, i) + str.substring(i + 1, n));
+                testPermutation("" + str.charAt(i), str.substring(0, i) + str.substring(i + 1, n));
             }
-        });
+        }
+    }
+
+    /**
+     * Test a potential word
+     * @param potentialWord String of letters
+     */
+    private static void checkIfWord(final String potentialWord)
+    {
+        // Pretty self-explanatory, if the string is a word, add it to the solutions!
+        if (mDictionary.contains(potentialWord))
+        {
+            mSolutions.add(potentialWord);
+        }
+    }
+
+    /**
+     * Not the most elegant way of displaying the results, but I don't think that was the point.
+     */
+    private void displayWords()
+    {
+        String resultDisplay = "";
+
+        for(String word: mSolutions)
+        {
+            resultDisplay += (resultDisplay.isEmpty()) ? word : "\n" + word;
+        }
+
+        display(resultDisplay);
     }
 }
